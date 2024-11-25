38e3029b1423315ecf91c88185f45c2c98c3633f
aemzed <adityazunaidi115@gmail.com>
2024-11-25 18:49:31 +0700

    first init

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace App\Controllers;
+
+class AuthController extends BaseController
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function authenticate()
+    {
+        // Authentication logic here
+    }
+}
